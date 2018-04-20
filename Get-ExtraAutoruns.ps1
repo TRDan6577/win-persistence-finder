7a8e6f077ae39b139d8cf26ee9e5b11d142e6a02
Tom Daniels <trd6577@g.rit.edu>
2018-04-19 20:00:51 -0400

    Fixed spelling. Added accessibility persistence

--- Get-ExtraAutoruns.ps1
+++ Get-ExtraAutoruns.ps1
@@ -1,4 +1,8 @@
-﻿Function Get-ScreenSaverRegKeys {
+﻿# Global variables
+$TypeReg = "RegistryKey"
+$TypeBin = "File"
+
+Function Get-ScreenSaverRegKeys {
     <#
     .SYNOPSIS
         This function grabs the registry key at 
@@ -8,7 +12,7 @@
         This function checks to see the registry key
         HKCU\Control Panel\Desktop\SCRNSAVE.exe exists. If it does exist, it
         checks to see if it is signed. When the function returns, it returns
-        a PersistanceFinderObject or null.
+        a PersistenceFinderObject or null.
 
     .LINK
         https://attack.mitre.org/wiki/Technique/T1180
@@ -20,23 +24,95 @@
 
     $Path = "HKCU:\Control Panel\Desktop\SCRNSAVE.EXE" # The path to the screen saver registry key
     $MatrixNumber = "T1180" # The Mitre Attack Matrix ID
-
-    try {
-        $KeyValue = $(Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -name 'SCRNSAVE.EXE' -ErrorAction Stop).'SCRNSAVE.EXE'
-        $SignatureResults = Get-AuthenticodeSignature -FilePath $KeyValue
-        $MD5 = $(Get-FileHash -Path $KeyValue -Algorithm MD5).Hash
-        $SHA1 = $(Get-FileHash -Path $KeyValue -Algorithm SHA1).Hash
-    
-        return New-PersistanceFinderObject -PersistanceMethod 'Screen Saver' -Path `
-        $Path -Value $KeyValue -MD5 $MD5 -SHA1 $SHA1 -ValidSignature `
-        $SignatureResults.Status -SignerCertificate $SignatureResults.SignerCertificate `
-        -AttackMatrixNumber $MatrixNumber
-    }catch{  # If the key doesn't exist, return null
-        return $null
-    }
+    return New-PersistenceFinderObject -PersistenceMethod "Screen Saver" -Type $TypeReg `
+        -Path $Path -AttackMatrixNumber $MatrixNumber
 }
 
-Function New-PersistanceFinderObject {
+Function Get-AccessibilityFeatures {
+<#
+    .SYNOPSIS
+        Gets the information about the accessibility binaries and registry keys
+
+    .DESCRIPTION
+        If an attacker has either physical access to a machine or RDP access
+        to a machine, putting a backdoor in the accessibility features
+        would allow easy re-entry to a system, even if the machine is on
+        the logon screen. This function will check if the following
+        accessibility binaries share an MD5 hash with any binaries that
+        could be used for persistence.
+
+        Accessibility Binaries:
+            sethc.exe         (Sticky-Keys)
+            utilman.exe       (Accessibility Menu)
+            osk.exe           (On-Screen Keyboard)
+            Magnify.exe       (magnifier)
+            Narrator.exe      (Narrator)
+            DisplaySwitch.exe (Display Switcher)
+            AtBroker.exe      (App Switcher)
+
+        Persistence Binaries (MD5 hash of accessibility should NOT match these):
+            explorer.exe              (Windows Explorer)
+            taskschd.msc              (Task Scheduler)
+            cmd.exe                   (Command Prompt)
+            powershell.exe            (Powershell)
+            powershell_ise.exe        (Powershell ISE)
+            regedit.exe               (Registry Editor)
+            Taskmgr.exe               (Task Manager)
+
+        <INSERT SOMETHING ABOUT REGISTRY KEYS HERE>
+
+    .LINK
+        https://attack.mitre.org/wiki/Technique/T1015
+
+    .NOTES
+        Author: Tom Daniels
+        License: MPL v2.0
+    #>
+
+    $MatrixNumber = "T1015"
+    $PersistenceMethod = "Accessibility Features"
+
+    # Accessibility Paths
+    $AccessibilityPaths = $($env:windir + "\system32\sethc.exe"), `
+        $($env:windir + "\system32\utilman.exe"), $($env:windir + "\system32\osk.exe"),`
+        $($env:windir + "\system32\Magnify.exe"), $($env:windir + "\system32\Narrator.exe"),`
+        $($env:windir + "\system32\DisplaySwitch.exe"), $($env:windir + "\system32\AtBroker.exe")
+
+    # Persistence Paths
+    $PersistencePaths = $($env:windir + "\System32\WindowsPowerShell\v1.0\powershell.exe"),`
+        $($env:windir + "\explorer.exe"), $($env:windir + "\system32\Taskmgr.exe"),`
+        $($env:windir + "\system32\taskschd.msc"),`
+        $($env:windir + "\System32\WindowsPowerShell\v1.0\powershell_ise.exe"),`
+        $($env:windir + "\regedit.exe"), $($env:windir + "\system32\cmd.exe")
+
+    # Calculate the hash for each persistence method
+    $PersistenceHashes = @()
+    For($i=0; $i -lt $PersistencePaths.Length; $i++){
+        $PersistenceHashes += Get-FileHash -Path $PersistencePaths[$i] -Algorithm MD5
+    }
+
+    # For each accessibility method, check to see if it matches an admin
+    # utility tool (see "Persistence Binaries" above in the function docs) 
+    $AccessibilityObjects = @()
+    ForEach ($Accessible in $AccessibilityPaths){
+        $PersistenceObject = New-PersistenceFinderObject `
+            -PersistenceMethod $PersistenceMethod -Type $TypeBin -Path `
+            $Accessible -AttackMatrixNumber $MatrixNumber
+        $PersistenceObject | Add-Member -MemberType NoteProperty -Name MatchesAdminUtil -Value "No"
+        
+        ForEach ($PersistenceHash in $PersistenceHashes){
+            if($PersistenceObject.MD5 -eq $PersistenceHash.Hash){
+                $PersistenceObject.MatchesAdminUtil = $PersistenceHash.Path
+                break
+            }
+        }
+        $AccessibilityObjects += $PersistenceObject
+    }
+
+    return $AccessibilityObjects
+}
+
+Function New-PersistenceFinderObject {
     <#
     .SYNOPSIS
         Makes a custom powershell object with the specified parameters.
@@ -47,30 +123,19 @@ Function New-PersistanceFinderObject {
         of that registry key. It is assumed that the value of the registry
         key is a binary. It will also hold whether or not the binary is signed
         by whom it is signed. Finally, it holds the Mitre Attack Matrix ID of
-        the persistance method.
+        the persistence method.
 
-    .PARAMETER PersistanceMethod
-        This is the name of the persistance method. For example, if a piece of
-        malware were to use a scheduled task as a persistance method, the value
+    .PARAMETER PersistenceMethod
+        This is the name of the persistence method. For example, if a piece of
+        malware were to use a scheduled task as a persistence method, the value
         of this parameter should be "Scheduled Task".
+
+    .PARAMETER Type
+        This tells us whether the persistence method involves a registry key or a
+        file. The values for this should always be either "RegistryKey" or "File".
 
     .PARAMETER Path
         The path to either the registry key or the binary.
-
-    .PARAMETER Value
-        If the path points to a registry key, this should be the value of that key.
-
-    .PARAMETER MD5
-        The MD5 hash value of the file
-
-    .PARAMETER SHA1
-        The SHA1 hash value of the file
-
-    .PARAMETER ValidSignature
-        Tells us if the binary in the path (or value) is signed.
-
-    .PARAMETER SignerCertificate
-        Contains the certificate of the entity that signed the binary
 
     .PARAMETER AttackMatrixNumber
         The ID of the Mitre Attack Matrix technique associated with this attack
@@ -79,41 +144,61 @@ Function New-PersistanceFinderObject {
         Author: Tom Daniels
         License: MPL v2.0
     #>
+
+    #TODO: Don't trust user. Check the inputs:
+    #     Make sure the path exists
+    #     Make sure the type is correct (if you can do this, then just set the type manually)
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory=$True)]
-        [string]$PersistanceMethod,
+        [string]$PersistenceMethod,
+
+        [Parameter(Mandatory=$True)]
+        [string]$Type,
 
         [Parameter(Mandatory=$True)]
         [string]$Path,
 
         [Parameter(Mandatory=$True)]
-        [string]$Value,
-
-        [Parameter(Mandatory=$True)]
-        [string]$MD5,
-
-        [Parameter(Mandatory=$True)]
-        [string]$SHA1,
-
-        [Parameter(Mandatory=$False)]
-        [string]$ValidSignature,
-
-        [Parameter(Mandatory=$False)]
-        [System.Security.Cryptography.X509Certificates.X509Certificate2]$SignerCertificate,
-
-        [Parameter(Mandatory=$True)]
         [string]$AttackMatrixNumber
     )
 
+    # After the following code block, $ShortPath holds the path to the file or registry location
+    # and $Value holds the name of the key or file. Example, if $Path = "C:\Windows\system32\calc.exe",
+    # $Value will be "calc.exe" and $ShortPath will be "C:\Windows\system32\"
+    $Value = $Path.Split("\")[-1]
+    $ShortPath = $Path.Substring(0, $Path.LastIndexOf($Value))
+
+    # If we're dealing with a registry key
+    if($Type -eq $TypeReg){
+        try{
+            # Get the value of the registry key
+            $Value = $(Get-ItemProperty -Path $ShortPath -name $Value -ErrorAction Stop).$Value
+
+            # Verify the binary's signature and compute the hash values
+            $SignatureResults = Get-AuthenticodeSignature -FilePath $Value
+            $MD5 = $(Get-FileHash -Path $Value -Algorithm MD5).Hash
+            $SHA1 = $(Get-FileHash -Path $Value -Algorithm SHA1).Hash
+        }catch{
+            # If the key doesn't exist, return $null
+            return $null
+        }
+    }else{ # Case if we're dealing with a binary file
+        $SignatureResults = Get-AuthenticodeSignature -FilePath $Path
+        $MD5 = $(Get-FileHash -Path $Path -Algorithm MD5).Hash
+        $SHA1 = $(Get-FileHash -Path $Path -Algorithm SHA1).Hash
+    }
+
+    # Create the object
     $object = New-Object -TypeName psobject
-    $object | Add-Member -MemberType NoteProperty -Name Name -Value $PersistanceMethod
+    $object | Add-Member -MemberType NoteProperty -Name Name -Value $PersistenceMethod
+    $object | Add-Member -MemberType NoteProperty -Name Type -Value $Type
     $object | Add-Member -MemberType NoteProperty -Name Path -Value $Path
     $object | Add-Member -MemberType NoteProperty -Name Value -Value $Value
     $object | Add-Member -MemberType NoteProperty -Name MD5 -Value $MD5
     $object | Add-Member -MemberType NoteProperty -Name SHA1 -Value $SHA1
-    $object | Add-Member -MemberType NoteProperty -Name ValidSignature -Value $ValidSignature
-    $object | Add-Member -MemberType NoteProperty -Name SignerCertificate -Value $SignerCertificate
+    $object | Add-Member -MemberType NoteProperty -Name ValidSignature -Value $SignatureResults.Status
+    $object | Add-Member -MemberType NoteProperty -Name SignerCertificate -Value $SignatureResults.SignerCertificate
     $object | Add-Member -MemberType NoteProperty -Name MitreAttackMatrixNo -Value $AttackMatrixNumber
     return $object
 }
